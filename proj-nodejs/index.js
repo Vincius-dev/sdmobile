c8b93013dfd5b55f8c5be79c2f3f869af3798555
vinicius-dev <viniciusgabfarias123@gmail.com>
2022-10-10 20:10:19 -0300

    5.7 (Dotenv)

--- proj-nodejs/index.js
+++ proj-nodejs/index.js
@@ -1,27 +1,50 @@
+require ('dotenv').config()
 const express = require ('express')
 //mysql é o nome de uma variável, pode ser qualquer coisa
 //mysql parece mais intuitivo do que mysql2
 const mysql = require('mysql2')
 const app = express()
 app.use(express.json())
+
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE} = process.env
+
 app.get('/medicos', (req, res) => {
     const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'teste',
-        database: 'hospital',
-        password: '1234'
+        host: DB_HOST,
+        user: DB_USER,
+        database: DB_DATABASE,
+        password: DB_PASSWORD
     })
     connection.query('SELECT * FROM tb_medico', (err, results, fields) => {
         res.json(results)
     })
 })
 
+app.post('/medicos', (req, res) => {
+    const connection = mysql.createConnection({
+        host: DB_HOST,
+        user: DB_USER,
+        database: DB_DATABASE,
+        password: DB_PASSWORD
+    })
+
+    const crm = req.body.crm
+    const nome = req.body.nome
+    const sql = "INSERT INTO tb_medico (crm, nome) VALUES (?, ?)"
+
+    connection.query(sql, [crm, nome], (err, results, fields) => {
+        console.log (results)
+        console.log(fields)
+        res.send('ok')
+    })
+})
+
 app.get('/pacientes', (req, res) => {
         const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'rodrigo',
-        database: 'hospital',
-        password: '1234'
+        host: DB_HOST,
+        user: DB_USER,
+        database: DB_DATABASE,
+        password: DB_PASSWORD
         })
     connection.query('SELECT * FROM tb_paciente', (err, results, fields) =>
     {
