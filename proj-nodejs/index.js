9c99a0a4b2bd3636ee1291838f2326e71898644a
vinicius-dev <viniciusgabfarias123@gmail.com>
2022-10-10 20:29:55 -0300

    5.9 (Pool de conexões)

--- proj-nodejs/index.js
+++ proj-nodejs/index.js
@@ -7,58 +7,51 @@ const app = express()
 app.use(express.json())
 
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE} = process.env
+const pool = mysql.createPool({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    //se todas as conexões estiverem ocupadas, novos solicitantes esperam numa fila
+    //se configurado com false, causa um erro quando recebe requisições e todas
+    //as conexões estão ocupadas
+    waitForConnections: true,
+    //no máximo 10 conexões. Elas são abertas sob demanda e não no momento de
+    //construção do pool
+    connectionLimit: 10,
+    //quantos solicitantes podem aguardar na fila? 0 significa que não há limite
+    queueLimit: 0
+})
 
 app.get('/medicos', (req, res) => {
-    const connection = mysql.createConnection({
-        host: DB_HOST,
-        user: DB_USER,
-        database: DB_DATABASE,
-        password: DB_PASSWORD
-    })
-    connection.query('SELECT * FROM tb_medico', (err, results, fields) => {
+    pool.query('SELECT * FROM tb_medico', (err, results, fields) => {
         res.json(results)
     })
 })
 
 app.post('/medicos', (req, res) => {
-    const connection = mysql.createConnection({
-        host: DB_HOST,
-        user: DB_USER,
-        database: DB_DATABASE,
-        password: DB_PASSWORD
-    })
-
     const crm = req.body.crm
     const nome = req.body.nome
     const sql = "INSERT INTO tb_medico (crm, nome) VALUES (?, ?)"
 
-    connection.query(sql, [crm, nome], (err, results, fields) => {
-        console.log (results)
-        console.log(fields)
-        res.send('ok')
-    })
+    pool.query(
+        sql,
+        [crm, nome],
+        (err, results, fields) => {
+            console.log (results)
+            console.log(fields)
+            res.send('ok')
+            
+        })
 })
 
 app.get('/pacientes', (req, res) => {
-        const connection = mysql.createConnection({
-        host: DB_HOST,
-        user: DB_USER,
-        database: DB_DATABASE,
-        password: DB_PASSWORD
-        })
-    connection.query('SELECT * FROM tb_paciente', (err, results, fields) =>
-    {
+    pool.query('SELECT * FROM tb_paciente', (err, results, fields) => {
         res.json(results)
     })
 })
 
 app.get('/consultas', (req, res) => {
-    const connection = mysql.createConnection({
-        host: DB_HOST,
-        user: DB_USER,
-        database: DB_DATABASE,
-        password: DB_PASSWORD
-    })
     const sql = `
     SELECT
     m.nome as nome_medico, c.data_hora, p.nome as nome_paciente
@@ -67,10 +60,10 @@ app.get('/consultas', (req, res) => {
     WHERE
     m.crm = c.crm AND c.cpf = p.cpf
     `
-    connection.query(
+    pool.query(
         sql,
         (err, results, fields) => {
-            res.json(results)
+        res.json(results)
         }
     )
 })
